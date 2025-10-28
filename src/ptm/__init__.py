93a6569f91aaa01008fe3a42b3210b7184555578
Jinyan Xu <phantom@zju.edu.cn>
2025-10-28 18:21:55 +0800

    fix package

--- src/ptm/__init__.py
+++ src/ptm/__init__.py
@@ -1,18 +1,18 @@
-from .syntax.strdiv import enable_str_truediv
+from .cli import main as cli_main
 from .syntax.param import Parameter
 from .syntax.include import include
 from .syntax.environ import environ
+from .syntax.strdiv import enable_str_truediv
 from .syntax.shell import exec_cmd, exec_cmd_stdout, exec_cmd_stderr, exec_cmd_stdout_stderr
 from .system.builder import builder, task, target, targets
-from .cli import main as cli_main
 
-__version__ = "0.1.0"
+
 __all__ = [
-    "Parameter", 
-    "include", 
-    "environ", 
+    "task", "target", "targets", "builder",
+    "Parameter",
+    "include",
+    "environ",
     "exec_cmd", "exec_cmd_stdout", "exec_cmd_stderr", "exec_cmd_stdout_stderr",
-    "builder", "task", "target", "targets"
 ]
 
 enable_str_truediv()
